3e0db1fefd1950e570442c2313473d23fdcc2672
komalnegah <146884249+komalnegah@users.noreply.github.com>
2025-02-20 12:16:32 -0500

    Add files via upload

--- frontend/Signup.js
+++ frontend/Signup.js
@@ -1,143 +1,106 @@
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Eye, EyeOff } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onSubmit = (data) => {
+        console.log(data);
+        navigate('/dashboard');
+    };
+
     return (
-
-        <>
-
-
-
-
-            <section className='sec-login'>
-                <div className='container'>
-
-                    <div className='row height-row'>
-
-                        <div className='col-6 col-top'>
-
-                            <div className='d-flex justify-content-center'>
-                                <div className='box-size'>
-
-                                    <div className='d-flex align-items-center box-pad'>
-                                        <img className='inline-block' src='/images/we.png'></img>
-
-                                    </div>
-
-
-
-                                    <p className='wel-txt'>
-                                        Welcome back
-
-                                    </p>
-                                    <p className='det-txt'>
-                                        Please enter your details
-                                    </p>
-
-                                    <form className='form-space'>
-
-                                        <label className='txt-lab'>
-                                            Full name
-                                        </label>
-
-                                        <input className='inp-set' placeholder='Enter your name'>
-                                        </input>
-
-                                        <label className='txt-lab txt-new'>
-                                            Email
-                                        </label>
-
-                                        <input className='inp-set' placeholder='Enter your email'>
-                                        </input>
-
-                                        <label className='txt-lab txt-new'>
-                                            Password
-                                        </label>
-
-                                        <input className='inp-set' placeholder='Enter your password'>
-                                        </input>
-
-
-                                        <button className='mt-4 btn-sign'>
-
-                                            Sign up
-                                        </button>
-
-                                        <p className='m-0 pt-2 pb-2 text-center'>Or</p>
-
-                                        <button className=' btn-sign btn-google'>
-                                            <img className='img-set' src='/images/google.png'></img>
-                                            Sign up with google
-                                        </button>
-                                        {/* other sign in buttons */}
-                                        {/*
-                                         <button className='mt-2 btn-sign btn-google'>
-                                            <img className='img-set' src='/images/microsoft.png'></img>
-                                            Sign up with microsoft
-                                        </button>
-
-                                        <button className='mt-2 btn-sign btn-google'>
-                                            <img className='img-set' src='/images/apple-logo.png'></img>
-                                            Sign up with apple
-                                        </button>  */}
-                                        {/* other sign in buttons */}
-                                        {/* <div className='d-flex w-100 justify-content-center mt-4 mb-4'>
-
-                                            <button className=' btn-ntr'>
-                                                <img className='img-set demo-icon' src='/images/apple-logo.png'></img>
-                                            </button>
-                                            <button className=' btn-ntr'>
-                                                <img className='img-set demo-icon' src='/images/google.png'></img>
-                                                </button>
-                                                
-                                            <button className='btn-ntr'>
-
-                                                <img className='img-set' src='/images/microsoft.png'></img>
-                                            </button>
-
-                                        </div> */}
-
-
-
-                                        <div className='box-end'>
-                                            <p className='mt-2 mr-2 mb-0 account-txt'>
-                                                Already have an account?
-
-                                            </p>
-                                            <a className='mb-0 mt-2 sign-txt link-line' href=''>
-                                                Login
-                                            </a>
-
-                                        </div>
-
-                                    </form>
-
-
+        <section className='sec-signup'>
+            <div className='container-fluid'>
+                <div className='row height-row'>
+                    <div className='col-6 col-top'>
+                        <div className='d-flex justify-content-center'>
+                            <div className='box-size'>
+                                <div className='d-flex align-items-center box-pad'>
+                                    <img className='inline-block' src='/images/we.png' alt="Logo" />
                                 </div>
+                                <p className='wel-txt'>Welcome back</p>
+                                <p className='det-txt'>Please enter your details</p>
 
-                            </div>
+                                <form className='form-space' onSubmit={handleSubmit(onSubmit)}>
+                                    <label className='txt-lab'>Full name</label>
+                                    <input 
+                                        type='text' 
+                                        className='inp-set' 
+                                        placeholder='Enter your name' 
+                                        {...register("name", { required: "Full name is required" })} 
+                                    />
+                                    {errors.name && <p className="error-text">{errors.name.message}</p>}
 
-                        </div>
+                                    <label className='txt-lab txt-new'>Email</label>
+                                    <input 
+                                        type='email' 
+                                        className='inp-set' 
+                                        placeholder='Enter your email' 
+                                        {...register("email", { 
+                                            required: "Email is required", 
+                                            pattern: {
+                                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                                                message: "Enter a valid email address"
+                                            }
+                                        })} 
+                                    />
+                                    {errors.email && <p className="error-text">{errors.email.message}</p>}
 
-                        <div className='col-6 box-back'>
+                                    <div className="relative" style={{ position: "relative" }}>
+                                        <label className="txt-lab txt-new">Password</label>
+                                        <input
+                                            type={showPassword ? "text" : "password"}
+                                            className="inp-set pr-10"
+                                            placeholder="Enter your password"
+                                            {...register("password", {
+                                                required: "Password is required",
+                                                minLength: {
+                                                    value: 6,
+                                                    message: "Password must be at least 6 characters long"
+                                                }
+                                            })}
+                                        />
+                                        <button
+                                            type="button"
+                                            className="absolute right-2 top-1/2 transform -translate-y-1/2"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                        >
+                                            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                                        </button>
+                                    </div>
+                                    {errors.password && <p className="error-text">{errors.password.message}</p>}
 
-                            <div>
-                                <img className='vec-img' src='/images/onlyvector.png'></img>
+                                    <button type="submit" className='mt-4 btn-sign'>Sign up</button>
 
+                                    <p className='m-0 pt-2 pb-2 text-center'>Or</p>
+
+                                    <button className='btn-sign btn-google' onClick={() => window.open('https://accounts.google.com/signup', '_blank')}>
+                                        <img className='img-set' src='/images/google.png' alt="Google" />
+                                        Sign up with Google
+                                    </button>
+
+                                    <div className='box-end'>
+                                        <p className='mt-2 mr-2 mb-0 account-txt'>Already have an account?</p>
+                                        <Link className='mb-0 mt-2 sign-txt link-line' to='/login'>Login</Link>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
+                    <div className='col-6 box-back'>
+                        <div>
+                            <img className='vec-img' src='/images/onlyvector.png' alt="Background" />
+                        </div>
+                    </div>
                 </div>
-            </section>
-
-
-
-        </>
-
-
+            </div>
+        </section>
     );
 }
 
